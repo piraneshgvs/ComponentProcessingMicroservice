c2fd50c09b55076faffe7c9c207a858cf12469b5
spran <spran@Piranesh>
2022-07-24 21:40:40 +0530

    First Commit

---
+++ ComponentProcessingMicroservice/src/main/java/com/cognizant/component/processing/ComponentProcessingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.component.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentProcessingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentProcessingMicroserviceApplication.class, args);
+	}
+
+}
