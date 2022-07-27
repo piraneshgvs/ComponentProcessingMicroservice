f15d4f68fde54855252d1f533f87c4facd401fea
spran <spran@Piranesh>
2022-07-27 23:52:53 +0530

    Refactored the code

--- ComponentProcessingMicroservice/src/main/java/com/cognizant/component/ComponentProcessingMicroserviceApplication.java
+++ ComponentProcessingMicroservice/src/main/java/com/cognizant/component/ComponentProcessingMicroserviceApplication.java
@@ -1,9 +1,11 @@
-package com.cognizant.component.processing;
+package com.cognizant.component;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ComponentProcessingMicroserviceApplication {
 
 	public static void main(String[] args) {
---
+++ ComponentProcessingMicroservice/src/main/java/com/cognizant/component/constant/GeneralConstant.java
@@ -0,0 +1,10 @@
+package com.cognizant.component.constant;
+
+public class GeneralConstant {
+	
+	public static final int INTEGRAL_PROCESSING_CHARGE = 500;
+	public static final int ACCESSORY_PROCESSING_CHARGE = 300;
+	public static final int INTEGRAL_PROCESS_DURATION = 5;
+	public static final int ACCESSORY_PROCESS_DURATION = 2;
+
+}
